66bcdb80827c5864438b2fdebacc00d629c81464
RaShubham <93131636+RaShubham@users.noreply.github.com>
2021-12-22 14:22:29 +0530

    Update Division.java

--- Practice01/Division.java
+++ Practice01/Division.java
@@ -6,14 +6,16 @@ public class Division {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			System.out.println("Dividend: ");	
 			Scanner p= new Scanner(System.in);
 			int a = p.nextInt();
+			
+			System.out.println("Divisor: ");
 			Scanner q= new Scanner(System.in);
 			int b = q.nextInt();
+			
 			int c = a/b;
 			int d = a%b;
-			System.out.println("Dividend = "+a);
-			System.out.println("Divisor = "+b);
 			System.out.printf("Quotient = "+c+" , ");
 			System.out.printf("Remainder = "+d);
 	}
