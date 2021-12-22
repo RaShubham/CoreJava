b457393b315596e6a155df16e72c3942ec93cbe9
RaShubham <93131636+RaShubham@users.noreply.github.com>
2021-12-22 13:53:05 +0530

    Create Division.java

---
+++ Practice01/Division.java
@@ -0,0 +1,15 @@
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 20;
+			int b = 3;
+			int c = a/b;
+			int d = a%b;
+			System.out.println("Dividend = "+a);
+			System.out.println("Divisor = "+b);
+			System.out.printf("Quotient = "+c+" , ");
+			System.out.printf("Remainder = "+d);
+	}
+
+}
